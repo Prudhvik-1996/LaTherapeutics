85b3cd50e7d109652900f3bf29cb4c53c15dd2cc
Prudhvik-1996 <prudhvik.1996@gmail.com>
2020-03-29 18:47:29 +0530

    mailing issue fix

--- app.py
+++ app.py
@@ -1,45 +1,49 @@
 from flask import render_template, Flask, url_for, redirect, request
-from flask_mail import Mail, Message
 import os
+import smtplib
+import traceback
 
 app = Flask(__name__)
 
-mail_settings = {
-	"MAIL_SERVER": 'smtp.gmail.com',
-	"MAIL_PORT": 465,
-	"MAIL_USE_TLS": False,
-	"MAIL_USE_SSL": True,
-	# Enable this for Dev
-	"MAIL_USERNAME": 'prudhvik.1996@gmail.com',
-	"MAIL_PASSWORD": 'A*1lavanya'
-	# Enable this for Prod
-	# "MAIL_USERNAME": 'prudhvik.1996@msitprogram.net',
-	# "MAIL_PASSWORD": 'msit12345'
-}
-
-app.config.update(mail_settings)
-mail = Mail(app)
-
 app.config['SECRET_KEY'] = 'PRUDHVIK'
+
+sender = 'prudhvik.1996@gmail.com'
+receivers = ['prudhvik.1996@gmail.com']
+
 
 @app.route('/', methods=['GET', 'POST'])
 def homepage():
 	if request.method == 'POST':
-		name = request.form['Name']
-		email = request.form['Email']
-		phone = request.form['Phone']
-		subject = "Someone from your blog - " + request.form['Subject']
-		message = request.form['Message'] + "\n\nEmail: " + email + "\n\nPhone: " + phone
+		name = request.form['name']
+		email = request.form['email']
+		phone = request.form['phone']
+		subject = "Someone from your blog - " + request.form['subject']
+		message = request.form['message']
+		message = "\n\nEmail: " + email + "\nPhone: " + phone + "\nSubject: " + subject + "\nMessage: " + message + "\n"
 		print(name, email, phone, subject, message)
-		with app.app_context():
-			msg = Message(
-				sender=email,
-				recipients=["prudhvik.1996@gmail.com"],
-				subject = subject,
-				body=message
-			)
-			mail.send(msg)
-		# return render_template('index.html', contactMeMessage = "success")
+		try:
+			# creates SMTP session 
+			s = smtplib.SMTP('smtp.gmail.com', 587) 
+			  
+			# start TLS for security 
+			s.starttls() 
+			  
+			# Authentication 
+			# s.login("prudhvik.1996@gmail.com", "A*1Lavanya") 
+			s.login("latherapeutics@gmail.com", "009160108108") 
+
+			# sending the mail 
+			s.sendmail("latherapeutics@gmail.com", "latherapeutics@gmail.com", message) 
+
+			# terminating the session 
+			s.quit() 
+
+			print("Successfully sent email")
+			return "success"
+		except:
+			traceback.print_exc()
+			return "failed to send email"
+		return "success"
 	else:
 		return render_template('index.html', contactMeMessage = "")
 
